111f18a1e59626adad998f8004a27d64dd80111f
Mogkc <carson.mogk@cox.net>
2019-04-18 14:06:51 -0700

    group projects now use handlebars each

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 module.exports = (app) => {
     app.get("/portfolio", (req, res) => {
-        res.render("portfolio", {});
+        res.render("portfolio", {
+            group: [
+                {
+                    name: "Solar Predictor",
+                    img: "assets/imgs/SolarPredictor.PNG",
+                    github: "https://github.com/Mogkc/Project1",
+                    deployed: "",
+                    description: ""
+                },
+                {
+                    name: "Actualize Yourself",
+                    img: "assets/imgs/ActualizeYourself.PNG",
+                    github: "https://github.com/Neuromancer93/project-two",
+                    deployed: "",
+                    description: ""
+                }
+            ],
+            personal: [
+                {}
+            ]
+        });
     });
-    
+
     app.get("*", (req, res) => {
         res.render("index", {});
     });
